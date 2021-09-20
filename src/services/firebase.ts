17994c734d42506f54205df9781eb456d96f6d5b
haivh <haivh@solashi.com>
2021-09-20 22:11:45 +0700

    init firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjZzf5-XK2rQFJIpGKLQO2M5i9c7ybRPg",
+  authDomain: "cat2local.firebaseapp.com",
+  projectId: "cat2local",
+  storageBucket: "cat2local.appspot.com",
+  messagingSenderId: "1062888587604",
+  appId: "1:1062888587604:web:6b4e5fe2db8e2f218d88c1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
